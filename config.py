25fb81e9178f54f0fc29acd49f90e31cd648893c
Samuel Ay-Bryson <samuel@ay-bryson.com>
2020-10-05 18:17:50 +0200

    Replace database with JSONL

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 import random
 import string
-import sqlite3
 
 from flask import Flask, url_for
 from flask_basicauth import BasicAuth
@@ -9,23 +8,16 @@ from dotenv import load_dotenv
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
-
 
 
 class Config:
-    """Set Flask configuration from .env file."""
 
     # General Config
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
 
-    # Database
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/aybry/Code/Python/lookup_hub/data/dictionary_good.db'
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
     # Basicauth
     BASIC_AUTH_USERNAME = 'language'
     BASIC_AUTH_PASSWORD = 'awesumpassword123'
+
