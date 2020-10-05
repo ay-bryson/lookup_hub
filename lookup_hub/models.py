dcea869811315a17fe5af28f3501794384481731
Samuel Ay-Bryson <samuel@ay-bryson.com>
2020-10-05 18:18:24 +0200

    Write classes for database/lines

--- lookup_hub/models.py
+++ lookup_hub/models.py
@@ -1,51 +1,98 @@
-from . import db
+import os
+import json
+import uuid
+
+from redis import Redis
+from rq import Queue, Worker, Connection
+
+from config import basedir
 
 
-class Entry(db.Model):
-    
-    __tablename__ = 'lookup_hub'
-    
-    id = db.Column(
-        db.Integer,
-        primary_key = True
-    )
-    
-    en = db.Column(
-        db.String(),
-        index=False,
-        unique=False
-    )
-    
-    de = db.Column(
-        db.String(),
-        index=False,
-        unique=False
-    )
-    
-    nl = db.Column(
-        db.String(),
-        index=False,
-        unique=False
-    )
-    
-    en_c = db.Column(
-        db.String(),
-        index=False,
-        unique=False
-    )
-    
-    de_c = db.Column(
-        db.String(),
-        index=False,
-        unique=False
-    )
-    
-    nl_c = db.Column(
-        db.String(),
-        index=False,
-        unique=False
-    )
-    
-    def __repr__(self):
-        return f'<Dictionary entry {self.en}>'
-    
+redis_conn = Redis()
+q = Queue(connection=redis_conn)
+w = Worker(q)
+
+
+class Database(object):
+
+    data = []
+    indices = []
+
+    jsonl_fp = os.path.join(basedir, 'data', 'dictionary.jsonl')
+
+    def __init__(self):
+        with open(self.jsonl_fp, 'r') as jsonl_fp:
+            for jsonline in jsonl_fp:
+                line = Line(jsonline)
+                self.data.append(line)
+                self.indices.append(line.id)
+
+        self.last = None
+
+    def __getitem__(self, key):
+        # If using uuid
+        if isinstance(key, str):
+            index = self.indices.index(key)
+            return self.data[index].data
+
+        # If slicing/indexing
+        try:
+            return self.data[key].data
+        except:
+            return [line.data for line in self.data[key]]
+
+    def __setitem__(self, key, contents):
+        index = self.indices.index(key)
+        self.data[index].data.update(contents)
+
+        self._save()
+
+    def _save(self):
+        q.enqueue(self.update_jsonl, self.data)
+
+    def new_row(self, key):
+        index = self.indices.index(key)
+        blank_line = Line()
+        self.data.insert(index, blank_line)
+        self.indices.insert(index, blank_line.id)
+        self.last_id = blank_line.id
+
+        self._save()
+
+    def remove_row(self, key):
+        index = self.indices.index(key)
+        self.data.pop(index)
+        self.indices.pop(index)
+
+        self._save()
+
+    def update_jsonl(self, data):
+        with open(self.jsonl_fp, 'w') as jsonl_f:
+            for entry in data:
+                jsonl_f.writelines(json.dumps(entry.data) + '\n')
+
+
+class Line(object):
+
+    def __init__(self, contents=None):
+
+        if contents is None:
+            self.data = self.get_empty_dict()
+        elif isinstance(contents, dict):
+            self.data = contents
+        else:
+            self.data = json.loads(contents)
+
+        if self.data.get('id') is not None:
+            self.id = self.data.get('id')
+        else:
+            self.id = str(uuid.uuid4())
+            self.data.update({'id': self.id})
+
+
+    def get_empty_dict(self):
+        return {
+            'en': {'text': None, 'comment': None},
+            'de': {'text': None, 'comment': None},
+            'nl': {'text': None, 'comment': None},
+        }
