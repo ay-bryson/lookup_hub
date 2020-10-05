587b0c018ace3fc9df8f3fecf1a953b4c79ec43c
Samuel Ay-Bryson <samuel@ay-bryson.com>
2020-10-05 18:19:06 +0200

    Write object prototypes also on JS side

--- lookup_hub/static/scripts/dictionary.js
+++ lookup_hub/static/scripts/dictionary.js
@@ -1,35 +1,205 @@
-function createInput(elem) {
-    var key = $(elem).attr("data-key");
-    var targetLang = $(elem).attr("data-target-language");
-    var text = $(elem).text();
+var dictionary;
 
-    var newInput = $("<input class='cell-input'></input>");
-    newInput.attr("data-key", key);
-    newInput.attr("data-target-language", targetLang);
 
-    newInput.keyup( function(event) {
-        if (event.which == 13) {
-            updateCell(key, targetLang, event.target.value);
-            closeInputs();
+class Dictionary {
+    constructor() {
+        this.entries = [];
+        this.ids = [];
+    }
+
+    initialise(data) {
+        for (data of dictionaryJSONL) {
+            var entry = new Entry(data);
+            this.addRow(entry);
         }
-    })
+    }
 
-    newInput.val(text.trim());
-    $(elem).parent().append(newInput);
-    $(elem).hide();
-    newInput.focus();
+    addRow(entry, atID) {
+        this.push(entry);
+        this.insert(entry, atID);
+    }
+
+
+    push(entry) {
+        this.entries.push(entry);
+        this.ids.push(entry.id);
+    }
+
+    insert(entry, atID, index) {
+        if (atID === undefined) {
+            // Only here when initiating dictionary
+            $("#hub-grid").append(entry.html);
+        } else {
+            // atID specified => insert before `atID`
+            if (index === undefined) {
+                var index = this.ids.indexOf(atID);
+            }
+            this.entries.splice(index, 0, entry);
+            this.ids.splice(index, 0, entry.id);
+
+            var nextID = this.ids[index + 1];
+            var dictIDElems = findEntryElem(nextID);
+            $(entry.html).insertBefore(dictIDElems.first().parent());
+        }
+
+    }
+
+    remove(entryID) {
+        var index = this.ids.indexOf(entryID);
+
+        this.ids.splice(index, 1);
+        this.entries.splice(index, 1);
+
+        $("[data-row-id='" + entryID + "']").remove();
+    }
+
+    replace(entry) {
+        var index = this.ids.indexOf(entry.id);
+        this.remove(entry.id);
+        this.insert(entry, entry.id, index);
+    }
 }
 
 
-function closeInputs() {
-    $($(".hub-entry > input").siblings()).show();
-    $(".hub-entry > input").remove();
+class Entry {
+    constructor(data) {
+        this.id = data["id"];
+
+        this.textEN = emptyIfNull(data["en"]["text"]);
+        this.textDE = emptyIfNull(data["de"]["text"]);
+        this.textNL = emptyIfNull(data["nl"]["text"]);
+
+        this.comments = {
+            "en": data["en"]["comment"],
+            "de": data["de"]["comment"],
+            "nl": data["nl"]["comment"],
+        }
+
+    }
+
+    get html() {
+        return entryHTML(this);
+    }
+
+    commentHTML(language) {
+        var comment = this.comments[language];
+        if ([undefined, null, ""].indexOf(comment) == -1) {
+            return `
+                <div class="entry-comment" title="${ comment }">
+                    <i class="fas fa-comment-alt fa-lg"></i>
+                </div>
+            `
+        } else {
+            return ``
+        }
+    }
 }
+
+
+dictionary = new Dictionary();
+
+
+function emptyIfNull(string) {
+    if (string === null) {
+        return "";
+    } else {
+        return String(string);
+    }
+}
+
+
+var escape = document.createElement('textarea');
+function escapeHTML(html) {
+    escape.textContent = html;
+    return escape.innerHTML;
+}
+
+
+const entryHTML = (entry) => `
+    <div class="grid-elem buttons-left"
+        data-row-id="${ entry.id }">
+        <button
+            class="dictionary-button"
+            data-key="${ entry.id }"
+            onclick="removeRow('${ entry.id }');">
+            <i class="fas fa-minus-square fa-lg"></i>
+        </button>
+        <button
+            class="dictionary-button"
+            data-key="${ entry.id }"
+            onclick="appendRow('${ entry.id }');">
+            <i class="fas fa-plus-square fa-lg"></i>
+        </button>
+    </div>
+
+    <div class="grid-elem hub-entry de"
+        data-row-id="${ entry.id }">
+        <div class="hub-entry-text"
+            id="${ entry.id }-de"
+            data-key="${ entry.id }"
+            data-target-language="de">
+            ${ escapeHTML(entry.textDE) }
+        </div>
+        ` + entry.commentHTML("de") + `
+    </div>
+    <div class="grid-elem hub-entry en"
+        data-row-id="${ entry.id }">
+        <div class="hub-entry-text"
+            id="${ entry.id }-en"
+            data-key="${ entry.id }"
+            data-target-language="en">
+            ${ escapeHTML(entry.textEN) }
+        </div>
+        ` + entry.commentHTML("en") + `
+    </div>
+    <div class="grid-elem hub-entry nl"
+        data-row-id="${ entry.id }">
+        <div class="hub-entry-text"
+            id="${ entry.id }-nl"
+            data-key="${ entry.id }"
+            data-target-language="nl">
+            ${ escapeHTML(entry.textNL) }
+        </div>
+        ` + entry.commentHTML("nl") + `
+    </div>
+
+    <div class="grid-elem buttons-right"
+        data-row-id="${ entry.id }">
+        <button
+            data-key="${ entry.id }"
+            class="dictionary-button"
+            onclick="getEntry('${ entry.id }');">
+            <i class="fas fa-pen-square fa-lg"></i>
+        </button>
+    </div>`
 
 
 // Close all inputs on escape key
 $(document).keydown(function(event) {
     if (event.keyCode == 27) {
-        closeInputs();
+        $("#popup-container").hide();
     }
 });
+
+
+$(document).ready( function() {
+
+    dictionary.initialise();
+
+    $("#popup-container").on("click", function(event) {
+        if (event.target == $("#popup-container")[0]) {
+            $("#popup-container").hide();
+        }
+    })
+
+    $("#submit-entry-button").on("click", function() {
+        submitChanges();
+        $("#popup-container").hide();
+    })
+
+    $(".edit-entry-row > input").on("keyup", function(event) {
+        if (event.keyCode == 13) {
+            $("#submit-entry-button").click();
+        }
+    })
+})
--- lookup_hub/static/scripts/socket.js
+++ lookup_hub/static/scripts/socket.js
@@ -1,10 +1,11 @@
 var socket;
+var currentEntry;
 
 
 function startSocket() {
     socket = io.connect("http://" + document.domain + ":" + location.port);
 
-    socket.on("connect", () => {
+    socket.on("pong", () => {
         $("#socket-status").text("Connected").removeClass("disconnected").addClass("connected");
     })
 
@@ -12,21 +13,123 @@ function startSocket() {
         $("#socket-status").text("Disconnected").removeClass("connected").addClass("disconnected");
     })
 
-    socket.on("updated", data => {
-        $("#" + data["id"]).text(data["value"]);
+    socket.on("got_entry", data => {
+        currentEntry = data["entry_id"];
+        showEditWindow(data);
+    })
+
+    socket.on("updated_entry", (data) => {
+        var entry = new Entry(data["new_entry"])
+
+        dictionary.replace(entry)
+    })
+
+    socket.on("inserted_row", (data) => {
+        var atID = data["entry_id"];
+        var entry = new Entry(data["new_entry"]);
+        dictionary.addRow(entry, atID);
+    })
+
+    socket.on("removed_row", (data) => {
+        var atID = data["entry_id"];
+        dictionary.remove(atID);
     })
 }
 
 
+function updateDictionary(entryID, data) {
+    findEntryElem(entryID, "de").text(data["new_entry"]["de"]["text"]);
+    findEntryElem(entryID, "en").text(data["new_entry"]["en"]["text"]);
+    findEntryElem(entryID, "nl").text(data["new_entry"]["nl"]["text"]);
+}
 
-function updateCell(key, target, text) {
+
+
+function findEntryElem(entryID, targLang) {
+    var allMatches = $("[data-key='" + entryID + "']");
+
+    if (targLang === undefined) {
+        return allMatches;
+    } else {
+        return allMatches.filter( function() {
+            return $(this).attr('data-target-language') == targLang;
+        });
+    }
+}
+
+
+function getEntry(entryID) {
     var data = {
-        rowid: key,
-        target: target,
-        value: text,
+        entry_id: entryID,
+    };
+
+    socket.emit("get_entry", data);
+}
+
+
+function showEditWindow(data) {
+    $("#popup-container").show();
+    for (lang of ["en", "de", "nl"]) {
+        for (item of ["text", "comment"]) {
+            try {
+                $("#" + item + "-" + lang).val(data["entry"][lang][item]);
+            } catch(error) {
+                print(error)
+            }
+        }
+    }
+}
+
+
+function nullIfEmpty(string) {
+    if (string == "") {
+        return null;
+    } else {
+        return String(string);
+    }
+}
+
+
+function submitChanges() {
+    newEntry = {
+        en: {
+            text: $("#text-en").val(),
+            comment: nullIfEmpty($("#comment-en").val()),
+        },
+        de: {
+            text: $("#text-de").val(),
+            comment: nullIfEmpty($("#comment-de").val()),
+        },
+        nl: {
+            text: $("#text-nl").val(),
+            comment: nullIfEmpty($("#comment-nl").val()),
+        },
     }
 
-    socket.emit("update_cell", data);
+    var data = {
+        entry_id: currentEntry,
+        new_entry: newEntry,
+    }
+
+    socket.emit("update_entry", data);
+}
+
+
+function removeRow(elemID) {
+    var data = {
+        entry_id: elemID,
+    }
+
+    socket.emit("remove_row", data);
+}
+
+
+function appendRow(elemID) {
+    var data = {
+        entry_id: elemID,
+    }
+
+    socket.emit("new_row", data);
 }
 
 
